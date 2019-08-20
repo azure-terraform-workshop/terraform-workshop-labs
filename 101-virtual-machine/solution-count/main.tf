eecd54d7a47a784be55981cdb8d828b8000d116d
Tom Straub <tom.straub@insight.com>
2019-08-20 14:37:04 -0500

    Add count example

---
+++ 101-virtual-machine/solution-count/main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_version = ">= 0.12.6"
+  required_providers {
+    azurerm = "= 1.31"
+  }
+}
+
+variable "vm_count" {
+  default = 2
+}
+
+variable "prefix" {
+  default = "PREFIX-vm"
+}
+
+variable "location" {
+  default = "centralus"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name  
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.1.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic-${count.index}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  count = var.vm_count
+
+  ip_configuration {
+    name                          = "config1"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = azurerm_public_ip.main[count.index].id
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm-${count.index}"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main[count.index].id]
+  vm_size               = "Standard_A2_v2"
+  count = var.vm_count
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "${var.prefix}vm-osdisk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${var.prefix}vm${count.index}"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_windows_config {}
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-pubip-${count.index}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  count = var.vm_count
+}
+
+# output "private-ip" {
+#   value       = azurerm_network_interface.main.*.private_ip_address
+#   description = "Private IP Address"
+# }
+
+# output "public-ip" {
+#   value       = azurerm_public_ip.main.*.ip_address
+#   description = "Public IP Address"
+# }
